311588f287a5eab21d16f91855d594fdb449e572
lemon-lime-honey <bs0308@g.skku.edu>
2026-01-25 18:20:09 +0900

    feat: :sparkles: update structure

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,7 +4,8 @@ const blog = defineCollection({
   type: 'content',
   schema: z.object({
     title: z.string(),
-    date: z.coerce.date(),
+    pubDate: z.date(),
+    updatedDate: z.date().optional(),
     tags: z.array(z.string()).default([]),
     excerpt: z.string().optional(),
     featured: z.boolean().default(false),
